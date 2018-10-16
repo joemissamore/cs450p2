70d2a856085f51404a6775f920be094303ce16be
joemissamore <joemissamore@live.com>
2018-10-15 23:35:23 -0700

    redirection > working

--- src/parsetools.c
+++ src/parsetools.c
@@ -88,72 +88,26 @@ void parse(char ** line_words, int num_words)
     // needs to be adjusted back to the end of the array (in bounds)
     i--; // need to back i up
 
-    // Allocating space for the double pointer
-    // +1 indicates space for the null terminator
-
-    // ls -lax = 2
-    // ls -l -a -x = 4
     char ** temp = (char **) malloc(((command_len + 1) * sizeof(char *))); 
-    // slicearray(temp, line_words, i - command_len + 1, i);
+    
     slicearray(temp, line_words, i - command_len + 1, i);
-    // printf("\n\ntemp: %s\n\n", temp[0]);
-    // printArray(temp, command_len);
-    // printf("\n\ntemp: %s\n\n", temp[command_len]);
+   
     
     commands[num_commands].command_string = temp;
     commands[num_commands].command_length = command_len;
     commands[num_commands].redirection = NULL;
     
-    // printf("printing the last command...\n");
-    // printArray(commands[num_commands].command_string, commands[num_commands].command_length);
-    // printf("end printing the last command...\n\n");
-    // printf("redirection after: %s\n", commands[num_commands].redirection);
-    // printf("i: %d\n", i);
-    // printf("command_len: %d\n", command_len);
-    // printf("command: \n");
-    // printArray(temp, command_len);
-    // printf("num_commands: %d\n", num_commands);
-
     num_commands++;
-    // DEBUG
-    // 
-    // printf("num_commands: %d\n", num_commands);
+   
 
 
 
 
     if (num_commands == 0) {
         printf("Please enter a command\n");
-    }
-    else if (num_commands == 1) {
-        pid_t pid = fork();
-        if (pid == 0)
-        {
-            // _execute(line_words, num_words, -1, -1);
-            // execvp(commands[0].command_string[0], commands[0].command_string);
-            int err_check = execvp(commands[0].command_string[0], commands[0].command_string);
-            if (err_check == -1) {
-                printf("Error with command: %s\n", line_words[0]);
-                printArray(commands[0].command_string, commands[0].command_length);
-            }
-        }
-        else if (pid < 0) {
-            printf("Error creating child process on command: %s", line_words[0]);
-            exit(1);
-        }
-        
-    }
-
-
-    
-    // This is where multiple commands will be executed
+    } 
     else {
-        
-        // int num_fds = (num_commands - 1) * 2;
 
-        // int pfd[num_fds]; // pfd[4] ?? 
-        // pipe(pfd);
-        // TODO: create fds dynamically
         int num_pipes = num_commands - 1;
         int num_pipe_ends = (num_pipes * 2) + 3;
         int pfds[num_pipes][2];
@@ -161,86 +115,55 @@ void parse(char ** line_words, int num_words)
         for (int x = 0; x < num_pipes; x++) {
             pipe(pfds[x]);
         }
-        // printf("num_commands: %d\n", num_commands);
-        // printf("num_pipes: %d\n", num_pipes);
-        // printf("num_pipe_ends: %d\n", num_pipe_ends);
 
-        // printf("num_fds: %d\n", num_fds);
-        // printf("IN ELSE STATEMENT\n");
-        int j = 0;
-        for (; j < num_commands; j++) {
-
-            // printf("j: %d\n", j);
-            // printf("\ncommand: \n");
-            // printArray(commands[j].command_string, commands[j].command_length);
-            // printf("\n\n");
+        for (int j = 0; j < num_commands; j++) {
 
             int saved;
             int pfd;
 
-            if (j == 0) {
-                bool WRITE_TO_FILE = false;
-                if (commands[j+1].redirection != NULL && strcmp(commands[j+1].redirection, ">") == 0) {
-                    fflush(stdout);
-                    saved = dup(1);
-                    pfd = open(commands[j+1].command_string[0], O_WRONLY | O_CREAT, 0777);
-                    dup2(pfd, 1);
-                    close(pfd);
+            
+            bool WRITE_TO_FILE = false;
+            if (commands[j].redirection != NULL && strcmp(commands[j].redirection, ">") == 0) {
+                printf("REDIRECTION >\n");
+                WRITE_TO_FILE = true;
+                fflush(stdout);
+                saved = dup(1);
+                pfd = open(commands[j+1].command_string[0], O_WRONLY | O_CREAT, 0777);
+                dup2(pfd, 1);
+                close(pfd);
 
-                    }
-                    if (fork() == 0) {
-                        if (WRITE_TO_FILE) {
-                            printf("WRITE_TO_FILE\n");
-                        }
-                        // dup2(pfds[0][1], 1); // write
-                        for (int y = 3; y < num_pipe_ends; y++) {
-                            close(y);
-                        }
-                        execvp(commands[j].command_string[0], commands[j].command_string);
-                    }
-
-
-                    fflush(stdout);
-                    dup2(saved, 1);
-                    close(saved);
-                    j++; // increment j past ( j < num_commands )
-
+            }
+            if (fork() == 0) {
+                if(!WRITE_TO_FILE && j == 0) {
+                    dup2(pfds[0][1], 1); // write
                 }
-            else { 
-                /*
-                If the number of commands is greater than 1 we need to handle it
-                */
-                if (strcmp(commands[j - 1].redirection, ">") == 0) {
-                    if (fork() == 0) {
-                        printf("REDIRECTION > \n");
-                        int fd = open(commands[j].command_string[0], O_WRONLY | O_CREAT, 0777);
-                        dup2(pfds[j - 1][0], 0); // read
-                        dup2(fd, 1); // write
-                    }
-                    
-
+                else if (!WRITE_TO_FILE && j < num_commands - 1) {
+                    dup2(pfds[j - 1][0], 0); // read
+                    dup2(pfds[j][1], 1); // write
+                }
+                else if (j == num_commands - 1) {
+                    dup2(pfds[j - 1][0], 0); // read
                 } 
-                else if (strcmp(commands[j - 1]. redirection, "|") == 0) {
-                    printf("PIPE |\n");
-                    if (fork() == 0) {
-                        // [1][0]
-                        dup2(pfds[j - 1][0], 0); // read
-
-                        if (j < num_commands - 1) {
-                            printf("writing to j!\n");
-                            printf("j: %d\n", j);
-                            printf("num_commands - 1: %d\n", num_commands - 1);
-                            dup2(pfds[j][1], 1); // write
-                        }
-                            
-                        for (int y = 3; y < num_pipe_ends; y++) {
-                            close(y);
-                        }
-                        execvp(commands[j].command_string[0], commands[j].command_string);
-                    }
+                // if you are on the last command...
+                // still need to read from prev
+                // pipes
+                // ls -la | grep file > writefile
+                else if (WRITE_TO_FILE && j == num_commands - 2) {
+                    dup2(pfds[j - 1][0], 0); // read
                 }
-                
-            } 
+                for (int y = 3; y < num_pipe_ends; y++) {
+                    close(y);
+                }
+                execvp(commands[j].command_string[0], commands[j].command_string);
+            }
+
+            if (WRITE_TO_FILE) {
+                fflush(stdout);
+                dup2(saved, 1);
+                close(saved);
+                // This should kick us out of the for loop
+                j++; // increment j past ( j < num_commands )
+            }
         }
         //  Close parent pipes
         for (int y = 3; y < num_pipe_ends; y++) {
